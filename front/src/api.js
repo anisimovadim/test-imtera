5d5c9a52c6e08fc82fd87d3f783cf35383741793
v.anisimov <v.anisimov@nactacredit.com>
2025-11-25 04:54:29 +0300

    Изменение URL

--- front/src/api.js
+++ front/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "http://83.166.246.157";
 axios.defaults.withCredentials = true;
 
 axios.interceptors.request.use(config => {
