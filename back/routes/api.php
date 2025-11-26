bc372b8a117fc0757c914d4b49ce68123c624b6a
v.anisimov <v.anisimov@nactacredit.com>
2025-11-26 03:50:53 +0300

    тест

--- back/routes/api.php
+++ back/routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
-Route::get('/test', [\App\Http\Controllers\AuthController::class], 'test');
+Route::get('/test', [\App\Http\Controllers\AuthController::class, 'test']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
